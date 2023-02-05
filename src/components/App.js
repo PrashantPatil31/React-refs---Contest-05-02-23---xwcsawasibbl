75376be8bea92b94c9906c15479378fc821bc451
Prashant Patil <88845868+PrashantPatil31@users.noreply.github.com>
2023-02-05 20:52:45 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,49 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React,{useState,useEffect,useRef} from 'react'
 import '../styles/App.css';
 const App = () => {
+  const[state1 , setState1] =useState("");
+  const[state2 , setState2] =useState("");
+  const[email, setEmail] =useState("");
+  const[pass, setPass] =useState("");
+  
+  const ref1 = useRef();
+  const ref2 = useRef();
 
-  //code here 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const inputEmailRef = useRef(null)
-  const inputPasswordRef = useRef(null)
-  const [userDetails, setUserDetails] = useState({})
-  const handleSubmit = () => {
-    if (email == '') {
-      inputEmailRef.current.focus()
-    }
-    else if (password == '') {
-      inputPasswordRef.current.focus()
-    }
-    else {
-      setUserDetails({
-        email,
-        password
-      })
-      setEmail('')
-      setPassword('')
-    }
-
+  function handleInput1(e){
+   setState1(e.target.value);
   }
+
+  function handleInput2(e){
+    setState2(e.target.value);
+  }
+
+   
+
+  function handlesubmit(){
+       if(!state1){
+        ref1.current.focus();
+       }
+       else if(!state2){
+        ref2.current.focus();
+       }
+       else{
+        setEmail( state1); 
+        setPass (state2); 
+        setState1("");
+        setState2("");
+       }
+  }
+
   return (
     <div id="main">
       Email
-      <input id="inputEmail" type="text" value={email} ref={inputEmailRef} onChange={(e) => setEmail(e.target.value)} /><br />
+      <input id="inputEmail" type="text" value={state1} ref={ref1} onChange={handleInput1}/><br/>
       Password
-      <input id="inputPassword" type="text" value={password} ref={inputPasswordRef} onChange={(e) => setPassword(e.target.value)} /><br />
-      <button id="submitButton" onClick={handleSubmit}>Submit</button><br />
-      <p id="emailText">Your Email : {userDetails.email}</p>
-      <p id="passwordText">Your Password : {userDetails.password}</p>
-
+      <input id="inputPassword" type="text" value={state2} ref={ref2} onChange={handleInput2}/><br/>
+      <button id="submitButton" onClick={handlesubmit}>Submit</button><br/>
+      <p id="emailText">Your Email : {email}</p>
+      <p id ="passwordText">Your Password : {pass}</p>
+      
     </div>
   )
 }
